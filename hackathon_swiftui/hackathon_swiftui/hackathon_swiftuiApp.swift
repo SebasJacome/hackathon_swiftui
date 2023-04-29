c310e8e420bcdd95dd54e963132dbff608f5087c
SebasJacome <0237105@up.mx>
2023-04-29 10:39:56 -0600

    Proyect createed

---
+++ hackathon_swiftui/hackathon_swiftui/hackathon_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackathon_swiftuiApp.swift
+//  hackathon_swiftui
+//
+//  Created by Sebastian Jacome on 29/04/23.
+//
+
+import SwiftUI
+
+@main
+struct hackathon_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
